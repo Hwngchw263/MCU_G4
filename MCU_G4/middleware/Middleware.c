3cd5b1a872d9c222da5fb4e30f286e33ef287b7e
annhtuan <tuananhbka123@gmail.com>
2024-07-10 01:52:14 +0700

    sua gpio

--- MCU_G4/middleware/Middleware.c
+++ MCU_G4/middleware/Middleware.c
@@ -34,38 +34,23 @@ void LED_OFF(ARM_GPIO_Pin_t pin){
 }
 
 void Button_Init(void) {
-//	GPIO_drv->GPIO_Setup(SW2,NULL);
-//	GPIO_drv->GPIO_Setup(SW3,NULL);
-//	GPIO_drv->GPIO_SetPullResistor (SW2,ARM_GPIO_PULL_UP);
-//	GPIO_drv->GPIO_SetPullResistor (SW3,ARM_GPIO_PULL_UP);
-//
-//	GPIO_drv->SetEventTrigger(SW2,ARM_GPIO_TRIGGER_EITHER_EDGE);
-//	GPIO_drv->SetEventTrigger(SW3,ARM_GPIO_TRIGGER_EITHER_EDGE);
-//	GPIO_drv->SetDirection(SW2,ARM_GPIO_INPUT);
-//	GPIO_drv->SetDirection(SW3,ARM_GPIO_INPUT);
-    PCC->PCCn[PCC_PORTC_INDEX] |= PCC_PCCn_CGC_MASK;
+	GPIO_drv->Setup(SW2,NULL);
+	GPIO_drv->Setup(SW3,NULL);
+	GPIO_drv->SetPullResistor (SW2,ARM_GPIO_PULL_UP);
+	GPIO_drv->SetPullResistor (SW3,ARM_GPIO_PULL_UP);
 
-    PORTC->PCR[12] = PORT_PCR_MUX(1) | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK | PORT_PCR_IRQC(0xB);
-    PORTC->PCR[13] = PORT_PCR_MUX(1) | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK | PORT_PCR_IRQC(0xB);
-    PTC->PDDR &= ~(1 << 12);
-    PTC->PDDR &= ~(1 << 13);
+	GPIO_drv->SetEventTrigger(SW2,ARM_GPIO_TRIGGER_EITHER_EDGE);
+	GPIO_drv->SetEventTrigger(SW3,ARM_GPIO_TRIGGER_EITHER_EDGE);
+	GPIO_drv->SetDirection(SW2,ARM_GPIO_INPUT);
+	GPIO_drv->SetDirection(SW3,ARM_GPIO_INPUT);
+//    PCC->PCCn[PCC_PORTC_INDEX] |= PCC_PCCn_CGC_MASK;
+//
+//    PORTC->PCR[12] = PORT_PCR_MUX(1) | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK | PORT_PCR_IRQC(0xB);
+//    PORTC->PCR[13] = PORT_PCR_MUX(1) | PORT_PCR_PE_MASK | PORT_PCR_PS_MASK | PORT_PCR_IRQC(0xB);
+//    PTC->PDDR &= ~(1 << 12);
+//    PTC->PDDR &= ~(1 << 13);
     // Enable Interrupts for PORTC
     NVIC_EnableIRQ(PORTC_IRQn);
 }
 
-void Timer_Init(void) {
-    //
- 	PCC->PCCn[PCC_LPIT_INDEX] &= ~PCC_PCCn_CGC(1u);
-	SCG->FIRCCSR &= ~SCG_FIRCCSR_FIRCEN_MASK;
-	SCG->FIRCDIV |= SCG_FIRCDIV_FIRCDIV2(1);
-	SCG->FIRCCSR |= SCG_FIRCCSR_FIRCEN(1);
-	PCC->PCCn[PCC_LPIT_INDEX] |= PCC_PCCn_PCS(0b011);
-	PCC->PCCn[PCC_LPIT_INDEX] |= PCC_PCCn_CGC(1u);
-	LPIT0->MCR |= (1<<LPIT_MCR_M_CEN_SHIFT) | (1<<LPIT_MCR_DBG_EN_SHIFT);
-    LPIT0->TMR[0].TCTRL |= LPIT_TMR_TCTRL_MODE(0u);
-    LPIT0->TMR[0].TVAL = 48000000; // Set timer value for 2 seconds (assuming 2MHz clock)
-   // LPIT0->TMR[0].TCTRL = LPIT_TMR_TCTRL_TIE_MASK; // Enable interrupt
-    LPIT0->MIER |= (1 << LPIT_MIER_TIE0_SHIFT);
-    // Enable Interrupts for LPIT0s
-    NVIC_EnableIRQ(LPIT0_Ch0_IRQn);
-}
+
